1194fd573b1a2f1c52c1dbfcd2d77d29b252103f
Chris Kelly <chris@ckellydesign.com>
2024-05-01 16:13:54 -0400

    Add "optional" column to 'categories' model–to distinguish optional expenses from fixed/mandatory expenses

--- categories/category.model.js
+++ categories/category.model.js
@@ -8,6 +8,7 @@ function model(sequelize) {
         category_title: { type: DataTypes.STRING, allowNull: false },
         category_type: { type: DataTypes.STRING, allowNull: false },
         parentId: { type: DataTypes.INTEGER, allowNull: true, defaultValue: null },
+        optional: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
         created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
         updated: { type: DataTypes.DATE },
         hidden: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 }
