d188da1fdd23e96a6b75db893da154c1ae1737b8
Chris Kelly <chris@ckellydesign.com>
2024-10-31 15:46:33 -0400

    Provisions for updating Category "optional" field from CategoryPlan form

--- categories/category.model.js
+++ categories/category.model.js
@@ -8,7 +8,7 @@ function model(sequelize) {
         category_title: { type: DataTypes.STRING, allowNull: false },
         category_type: { type: DataTypes.STRING, allowNull: false },
         parentId: { type: DataTypes.INTEGER, allowNull: true, defaultValue: null },
-        optional: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+        optional: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
         created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
         updated: { type: DataTypes.DATE },
         hidden: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 }
